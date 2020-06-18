cc1c98a5436bb7864fae73df8eff20c0a52442a7
Najeeb <naxibkhan@gmail.com>
2020-06-19 01:19:52 +0500

    Transaction list dynamic

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
+
 
 
 
@@ -11,7 +13,7 @@ import './App.css';
 
 function App() {
   return (
-    <div >
+    <GlobalProvider >
      <Header />
      <div className="container">
         <Balance />
@@ -20,7 +22,7 @@ function App() {
         <AddTransaction />
 
      </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
