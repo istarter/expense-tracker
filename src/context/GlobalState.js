59993be6c1f8f3d0e2feb0cdb08c36eafc5bd390
Najeeb <naxibkhan@gmail.com>
2020-06-20 00:17:39 +0500

    resoved issue

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer, createContext } from "react";
+import React, {  useReducer, createContext } from "react";
 import AppReducer from './AppReducer'
 
 //initial state
