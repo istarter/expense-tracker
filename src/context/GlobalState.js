6a688c3bdd4adf9c55a241725330bd56aa19b37e
Najeeb <naxibkhan@gmail.com>
2020-06-19 23:02:12 +0500

    final commit

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,12 +3,7 @@ import AppReducer from './AppReducer'
 
 //initial state
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
-  ],
+  transactions: [],
 };
 
 //create context
@@ -26,11 +21,19 @@ function deleteTransaction(id) {
     });
 }
 
+function addTransaction(transaction) {
+  dispatch({
+    type: 'ADD_TRANSACTION',
+    payload: transaction
+  });
+}
+
 
 
   return (<GlobalContext.Provider value={{
       transactions:state.transactions,
-      deleteTransaction, 
+      deleteTransaction,
+      addTransaction 
   }}>
             { children }
   </GlobalContext.Provider>);
